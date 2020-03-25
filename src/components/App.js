746781c166643ab62776c0cf9887e0f5b577ea38
VukasinVujic <vukasinvujic021@gmail.com>
2020-03-25 12:02:59 +0100

    BrowserRouter, Route, Link

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import {BrowserRouter,Route, Link } from 'react-router-dom'
 
+const PageOne = () => {
+    return( <div>
+        Page one
+        <Link to="/pageTwo"> Navigate to page two</Link>
+        </div>
+    )
+}
+
+const PageTWo = () => {
+    return <div>Page two
+         <button>click me</button>
+         <Link to="/">Navigate to page one</Link>
+         </div>
+}
 
 const App = () => {
     return (
-        <div>App</div>
+        <div>
+            <BrowserRouter>
+            <div>
+                <Route path="/" exact component={PageOne}/>
+                <Route path="/pageTwo" component={PageTWo}/>
+
+            </div>
+            </BrowserRouter>
+
+        </div>
     )
 }
 
