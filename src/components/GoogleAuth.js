8fffebfbd352d0d6116039105cc97bd46f640516
VukasinVujic <vukasinvujic021@gmail.com>
2020-03-26 18:44:02 +0100

    load 'client:auth2',part of libr gapi,init client with Aouth client id, put instance of google auth obj in this.auth, change state.

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -2,19 +2,35 @@
 
  class GoogleAuth extends React.Component {
     
+    state = { isSignedIn: null};
+
     componentDidMount(){
-        window.gapi.load('client:auth2', () => {
-            window.gapi.client.init({
+        window.gapi.load('client:auth2', () => { // load gapi library
+            window.gapi.client.init({       // when it loadas it will call this arrow function and 
+                                            // initialize with oauth client id 
                 clientId: '861991510996-un7t04v8gg5equ299kauhu44c7upus4g.apps.googleusercontent.com',
                 scope: 'email'
-            })
+            }).then(()=>{   // since '.init' is async func. and return promise, use then to
+                this.auth = window.gapi.auth2.getAuthInstance(); // put an google aouth object in this class
+                this.setState({isSignedIn: this.auth.isSignedIn.get()}) // update state with function from aouth object
+            })                                                          // that is checking is user sign in
         });
     }
     
+    renderAuthButton(){
+        if(this.state.isSignedIn === null) {
+            return <div>I don't know if we are sign in</div>
+        } else if (this.state.isSignedIn){
+            return <div>I am signed in</div>
+        } else {
+            return <div>I am not signed in</div>
+        }
+    }
+
     render(){
          return (
              <div>
-                GoogleAuth
+                {this.renderAuthButton()}
              </div>
          )
      }
