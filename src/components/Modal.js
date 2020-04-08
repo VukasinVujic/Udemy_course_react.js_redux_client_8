ec4875cb7dbc953b3de36abf7df98e64dd3ad725
VukasinVujic <vukasinvujic021@gmail.com>
2020-04-08 15:03:27 +0200

    styling & stoping buble up onClick to parent

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import history from '../history';
 
 
 const Modal = props => {
     return ReactDom.createPortal(
-        <div className="ui dimmer modals visible active">
-             <div className="ui standard modal visible active">
-                 gde si ti , gde si ti ,
+        // every time the modal shows up , and user just click outside of it , he will be rerouted to home
+        <div 
+        onClick={ () => history.push('/')}
+        className="ui dimmer modals visible active"
+        >
+            {/* e.stopPropagation we use to stop event onClick to posible buble up to onClick in parent element  */}
+             <div
+             onClick={(e)=> e.stopPropagation()}
+            className="ui standard modal visible active"
+              >
+    {/* everything from header till ui cancel button is from Semantic-ui Modules/Modal/Actions */}
+                <div className="header">Delete Stream</div>    
+                <div className="content">
+                    <p>Are you sure you want to delete this stream</p>
+                </div>
+                 <div className="actions">
+                     <div className="ui primary button">Delete</div>
+                     <div className="ui button">Cancel</div>
+                 </div>
              </div>
         </div>,
         document.querySelector('#modal')
